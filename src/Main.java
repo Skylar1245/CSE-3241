0c028700794870121f0d5f88c92283e7aabb45e8
Skylar Stephens <myloslayer1245@gmail.com>
2024-04-01 10:29:27 -0400

    Correct Connecting to DB

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import db.DataManager;
@@ -10,9 +15,16 @@ import java.util.function.Consumer;
  * 
  * @authors Skylar Stephens, Kate Goertz, Haley Bonidie, Gwyn Barnholtz
  * 
- * https://generatedata.com/generator
+ *          https://generatedata.com/generator
  */
+
 public class Main {
+    // JDBC strings
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:####/FIXME";
+    static final String USER = "username";
+    static final String PASS = "password";
+    // Temporary list of people
     public static ArrayList<Person> People;
 
     /**
@@ -31,12 +43,46 @@ public class Main {
             ManageEquipment::Menu, ManageDatabase::Menu);
 
     /**
+     * Tests the database connection with a simple query
+     */
+    public static void TestDatabase() {
+        // JDBC
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            String sql = "SELECT id, first, last FROM Employees";
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, 1235550987);
+            rSet = stmt.executeQuery();
+            while (rSet.next()) {
+                int id = rSet.getInt("id");
+                String first = rSet.getString("first");
+                String last = rSet.getString("last");
+                System.out.println("ID: " + id + ", First: " + first + ", Last: " + last);
+            }
+        } catch (SQLException sqlE) {
+            sqlE.printStackTrace();
+            return;
+        } catch (ClassNotFoundException classE) {
+            classE.printStackTrace();
+            return;
+        }
+    }
+
+    /**
      * Main method loop
      * 
      * @param args
      */
     public static void main(String[] args) {
         Utility.clearTerminal();
+        // !
+        TestDatabase();
+        // !
         // Read from existing files
         People = DataManager.ReadFromFiles();
         Scanner scanner = new Scanner(System.in);
